c3391dd00e6f7d65669c1fdc0ae86d5ee2d04b2a
Kavi17498 <kavindulakshan187@gmail.com>
2025-01-07 10:05:26 +0530

    working properly stage

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function isSuperUser()
+{
+    return $this->role === 'superuser';
+}
+
+
+
+
 }
